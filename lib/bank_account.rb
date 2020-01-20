cf35873c7accb59ab96b8f45f719f2dfb8a49dcc
Latricia Nickelberry <tricianickelberry@gmail.com>
2020-01-20 20:02:01 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,7 +1,27 @@
 class BankAccount
-  attr_accessor :name, :bankaccount
-  
-  def initialize (name, bankaccount)
-    @name =name
-    @bankaccount =bankaccount
+  attr_reader :name
+  attr_accessor :balance, :status
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(deposit_amount)
+    self.balance += deposit_amount
+  end
+
+  def display_balance
+    "Your balance is $#{balance}."
+  end
+
+  def close_account
+    self.status = "closed"
+  end
+
+  def valid?
+    balance > 0 && status == "open"
+  end
+
 end
